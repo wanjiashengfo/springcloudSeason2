5c2b58663349095be599311abd2f2803b992034c
wanjiashengfo <493000667@qq.com>
2020-04-03 02:23:11 +0800

    first

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/lb/MyLb.java
@@ -0,0 +1,27 @@
+package com.atguigu.springcloud.lb;
+
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Component
+public class MyLb implements LoadBalancer{
+    private AtomicInteger atomicInteger = new AtomicInteger(0);
+
+    public final int getAndIncrement(){
+        int current;
+        int next;
+        do{
+            current = this.atomicInteger.get();
+            next = current >= 2147483647 ? 0 :current+1;
+        }while(!this.atomicInteger.compareAndSet(current,next));
+        return next;
+    }
+    @Override
+    public ServiceInstance instances(List<ServiceInstance> serviceInstanceList) {
+        int i = getAndIncrement() % serviceInstanceList.size();
+        return serviceInstanceList.get(i);
+    }
+}
